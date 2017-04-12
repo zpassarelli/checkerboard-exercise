509a02f01b7f22db5b841df16fc285328366a9cc
Zach Passarelli <zachary.passarelli@gmail.com>
2017-04-12 16:42:22 -0600

    Completed gradient checkerboard

--- script.js
+++ script.js
@@ -11,9 +11,34 @@ function divgen(red,green,blue){
 }
 
 function randomColor() {
-  return Math.floor(Math.random() * 256);
+  return Math.floor(Math.random() * 255);
 }
 
-for(var i = 0; i <= 53; i++){
-  document.body.appendChild(divgen(randomColor(),randomColor(),randomColor()));
+function colorShift(color,dir){
+  if(dir === 'up'){
+    if(color + 10 > 255){
+      return (color + 10) - 255;
+    } else {
+      return color + 10;
+    }
+  } else {
+    if(color - 15 < 0){
+      return (color - 15) + 255;
+    } else {
+      return color - 15;
+    }
+  }
+}
+
+var grad1 = randomColor();
+var grad2 = randomColor();
+var grad3 = randomColor();
+
+for(var i = 0; i <= 20; i++){
+  document.body.appendChild(divgen(grad1,grad2,grad3));
+  grad1 = colorShift(grad1,'down');
+  document.body.appendChild(divgen(grad1,grad2,grad3));
+  grad1 = colorShift(grad1,'up');
+  document.body.appendChild(divgen(grad1,grad2,grad3));
+  grad1 = colorShift(grad1,'down');
 }
