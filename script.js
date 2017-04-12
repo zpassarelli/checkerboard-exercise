2b78f4c308b81a976fb2715d7ce3ff860268ef26
Zach Passarelli <zachary.passarelli@gmail.com>
2017-04-12 15:45:46 -0600

    Completed red and black checkerboard

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-// Your JS goes here
+var body = document.getElementsByTagName('body');
+
+function divgen(color){
+  var newDiv = document.createElement('div');
+  newDiv.style.backgroundColor = color;
+  newDiv.style.width = '11.1%';
+  newDiv.style.float = 'left';
+  newDiv.style.paddingBottom = '11.1%';
+
+  return newDiv;
+}
+
+for(var i = 0; i < 27; i++){
+  //for(var j = 0; j < 5; j++){
+    document.body.appendChild(divgen('red'));
+    document.body.appendChild(divgen('black'));
+  //}
+}
